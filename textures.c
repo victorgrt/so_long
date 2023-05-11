5762126f2bdeb7a1062b59ec675c146c12d0510d
Victor GORET <vgoret@e2r3p9.clusters.42paris.fr>
2023-05-11 17:38:26 +0200

    Ressources->Textures + norme

--- textures.c
+++ textures.c
@@ -6,7 +6,7 @@
 /*   By: vgoret <vgoret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 19:19:40 by victor            #+#    #+#             */
-/*   Updated: 2023/05/11 16:21:53 by vgoret           ###   ########.fr       */
+/*   Updated: 2023/05/11 17:35:26 by vgoret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,22 @@ void	init_img2ndpart(t_data *game)
 	int	img_y;
 
 	(*game).exit_colored = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/exit_colored.xpm", &img_x, &img_y);
+		"./textures/exit_colored.xpm", &img_x, &img_y);
 	game->textures[3] = game->exit_colored;
 	if (!(*game).exit_colored)
 		fail_xpm(game, 3);
 	(*game).player_left = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/player_left.xpm", &img_x, &img_y);
+		"./textures/player_left.xpm", &img_x, &img_y);
 	game->textures[5] = game->player_left;
 	if (!(*game).player_left)
 		fail_xpm(game, 4);
 	(*game).player_right = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/player_right.xpm", &img_x, &img_y);
+		"./textures/player_right.xpm", &img_x, &img_y);
 	game->textures[5] = game->player_right;
 	if (!(*game).player_right)
 		fail_xpm(game, 5);
 	(*game).collect = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/collect.xpm", &img_x, &img_y);
+		"./textures/collect.xpm", &img_x, &img_y);
 	game->textures[6] = game->collect;
 	if (!(*game).collect)
 		fail_xpm(game, 6);
@@ -61,17 +61,17 @@ void	init_img(t_data *game)
 	int	img_y;
 
 	(*game).wall = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/64skull.xpm", &img_x, &img_y);
+		"./textures/64skull.xpm", &img_x, &img_y);
 	game->textures[0] = game->wall;
 	if (!(*game).wall)
 		fail_xpm(game, 0);
 	(*game).floor = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/64floor.xpm", &img_x, &img_y);
+		"./textures/64floor.xpm", &img_x, &img_y);
 	game->textures[1] = game->floor;
 	if (!(*game).floor)
 		fail_xpm(game, 1);
 	(*game).exit_nc = mlx_xpm_file_to_image((*game).mlx, \
-		"./ressources/exit_nc.xpm", &img_x, &img_y);
+		"./textures/exit_nc.xpm", &img_x, &img_y);
 	game->textures[2] = game->exit_nc;
 	if (!(*game).exit_nc)
 		fail_xpm(game, 2);
